8faff9f6399bdf45c48e88c77efa796e528380b6
Johannes <office@atelierbuchner.at>
2024-05-02 10:58:47 +0200

    SEO and Google analytcis

--- components/SEO/index.jsx
+++ components/SEO/index.jsx
@@ -4,7 +4,7 @@ import Head from "next/head";
 const Meta = ({ data }) => {
     return (
         <Head>
-            <title>Produktentwicklung und Produktion | SinoScan DE</title>
+            <title>Ihre Anfrage für Produktentwicklung und Produktion | SinoScan DE</title>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta charSet="utf-8" />
 
